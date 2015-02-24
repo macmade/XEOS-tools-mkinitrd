ce3f2ae7a8885b1c4882260f8c50d164f8cabdd4
macmade <macmade@xs-labs.com>
2015-02-24 01:46:50 +0100

    Added the new copyright notices.

--- source/mkinitrd.c
+++ source/mkinitrd.c
@@ -59,6 +59,11 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
+/*!
+ * @author          Jean-David Gadina
+ * @copyright       (c) 2010-2015, Jean-David Gadina - www.xs-labs.com
+ */
+
 #include "mkinitrd.h"
 
 #define MKINITRD_CLEANUP            free( ( void * )filepaths );            \
